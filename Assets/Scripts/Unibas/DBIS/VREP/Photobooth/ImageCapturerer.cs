049570193afe01fb1bcf0f50659c2b1d7abc3b71
loris.sauter <loris.sauter@unibas.ch>
2019-09-08 09:56:42 +0200

    Added cropping and resizing of image

--- Assets/Scripts/Unibas/DBIS/VREP/Photobooth/ImageCapturerer.cs
+++ Assets/Scripts/Unibas/DBIS/VREP/Photobooth/ImageCapturerer.cs
@@ -22,9 +22,31 @@ namespace Unibas.DBIS.VREP.Photobooth
         {
             yield return new WaitForEndOfFrame();
             var tex = Renderer.material.mainTexture.Convert();
+            var newTex = RotateAndCrop(tex);
             byte[] bytes = tex.EncodeToPNG();
             Destroy(tex);
             handler.Invoke(bytes);
+        }
+
+        private Texture2D RotateAndCrop(Texture2D tex)
+        {
+            var newHeight = tex.height / 2;
+            Color[] pixels = new Color[newHeight*tex.width];
+            Texture2D resized = new Texture2D(tex.width, newHeight, TextureFormat.RGB24, false);
+
+            
+            for(int y = 0; y<newHeight; y++)
+            {
+                for (int x = 0; x < tex.width; x++)
+                {
+                    pixels[(newHeight-y) * tex.width + x] = tex.GetPixel(x, y + newHeight);
+                }
+            }
+            
+            resized.SetPixels(pixels);
+            resized.Apply();
+            Destroy(tex);
+            return resized;
         }
         
         
