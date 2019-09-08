4a495cdec62b3b4355bef8260620ff09a9a415fe
loris.sauter <loris.sauter@unibas.ch>
2019-09-08 10:32:58 +0200

    Added trigger script

---
+++ Assets/Scripts/Unibas/DBIS/VREP/Photobooth/PhotoCaptureTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace Unibas.DBIS.VREP.Photobooth
+{
+    public class PhotoCaptureTrigger : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean grabPinch; //Grab Pinch is the trigger, select from inspecter
+        public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;//which controller
+        // Use this for initialization
+ 
+        void OnEnable ()
+        {
+            if (grabPinch != null)
+            {
+                grabPinch.AddOnChangeListener(OnTriggerPressedOrReleased, inputSource);
+            }
+        }
+ 
+ 
+        private void OnDisable()
+        {
+            if (grabPinch != null)
+            {
+                grabPinch.RemoveOnChangeListener(OnTriggerPressedOrReleased, inputSource);
+            }
+        }
+ 
+ 
+        private void OnTriggerPressedOrReleased(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState)
+        {
+            
+            Debug.Log("Trigger was pressed or released, state="+newState);
+        }
+    }
+}
